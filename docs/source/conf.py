550715564b8ba14659e098fca49d63073c0cd5d5
MoseyQAQ <admin@aleeqaq.cc>
2025-05-03 18:50:58 +0800

    1. feat(doc): automate API renference generation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,3 +1,7 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../src/ferrodispcalc'))
+
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -17,7 +21,11 @@ release = '1.0'
 extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.viewcode',
               'sphinx.ext.githubpages',
-              'myst_parser']
+              'myst_parser',
+              'sphinx.ext.napoleon',
+              'sphinx.ext.todo',
+              'sphinx.ext.coverage',
+              'sphinx.ext.mathjax',]
 
 templates_path = ['_templates']
 exclude_patterns = []
