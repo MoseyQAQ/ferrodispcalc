7f53c88276757490b2b21b04d29059c5074c1bb2
MoseyQAQ <admin@aleeqaq.cc>
2025-08-04 13:00:55 +0800

    feat: add match_atoms function for atom matching and distance calculation

--- src/ferrodispcalc/utils/utils.py
+++ src/ferrodispcalc/utils/utils.py
@@ -1,5 +1,7 @@
 from ase import Atoms 
 import numpy as np
+from tqdm import tqdm
+from ase.calculators.singlepoint import SinglePointCalculator
 
 def get_polarization_quanta(atoms: Atoms) -> np.ndarray:
     '''
@@ -92,3 +94,54 @@ def l2c(atoms: Atoms, vector: np.ndarray) -> np.ndarray:
         The vector in crystal lattice coordinates to be converted.
     '''
     return crystal_lattice_to_cartesian(atoms, vector)
+
+def match_atoms(ref_atoms: Atoms, atoms: Atoms) -> Atoms:
+    ref_cell = ref_atoms.cell.array.copy()
+    atoms_cell = atoms.cell.array.copy()
+
+    assert np.allclose(ref_cell, atoms_cell), "Cells do not match, please check the input Atoms objects."
+    assert len(ref_atoms) == len(atoms), "Number of atoms do not match, please check the input Atoms objects."
+    
+    inv_cell = np.linalg.inv(ref_cell)
+
+    ref_coords = ref_atoms.get_positions()
+    coords = atoms.get_positions()
+
+    mapping_idx = np.zeros(len(ref_atoms), dtype=int) # map: ref_atoms -> atoms
+    max_dist_sq = None
+    for i in tqdm(range(len(ref_atoms)), desc="Matching atoms"):
+        ref_coord = ref_coords[i]
+
+        # Calculate the displacement in fractional coordinates
+        displacement = coords - ref_coord
+        displacement_frac = displacement @ inv_cell
+        displacement_frac -= np.round(displacement_frac)
+        displacement = displacement_frac @ ref_cell
+        dist_sq = np.sum(displacement**2, axis=-1)
+
+        # Find the index of the closest atom
+        min_idx = np.argmin(dist_sq)
+        if max_dist_sq is None or dist_sq[min_idx] > max_dist_sq:
+            max_dist_sq = dist_sq[min_idx]
+        mapping_idx[i] = min_idx
+
+    assert len(set(mapping_idx)) == len(ref_atoms), "Some atoms in the reference structure do not have a unique match in the target structure."
+    
+    print(f"Minimum distance squared between matched atoms: {max_dist_sq:.3f} A^2")
+    
+    # return a new Atoms object with the matched atoms
+    _atoms = atoms.copy()
+    _atoms = _atoms[mapping_idx]
+
+    # Attach the new calculator
+    ## Note: only support forces and energy.
+    e = atoms.get_potential_energy()
+    f = atoms.get_forces()
+    f_reordered = f[mapping_idx]
+    sg = SinglePointCalculator(
+        _atoms,
+        energy=e,
+        forces=f_reordered,
+    )
+    _atoms.set_calculator(sg)
+    return _atoms
